e807bf9e20dada2de69e33f0fb9931cd58015319
Nawaz <mpnkhan@gmail.com>
2014-10-16 12:19:02 +0530

    changing carousel markup as per latest HTML base

--- plugins/js/bootstrap-accessibility.js
+++ plugins/js/bootstrap-accessibility.js
@@ -48,45 +48,6 @@
 $('.alert').attr('role', 'alert')
 $('.close').removeAttr('aria-hidden').wrapInner('<span aria-hidden="true"></span>').append('<span class="sr-only">Close</span>')
 
-  // TOOLTIP Extension
-  // ===============================
-  
-    var showTooltip =    $.fn.tooltip.Constructor.prototype.show
-        , hideTooltip =    $.fn.tooltip.Constructor.prototype.hide
-
-    $.fn.tooltip.Constructor.prototype.show = function () {
-        showTooltip.apply(this, arguments)
-        var $tip = this.tip()
-            , tooltipID = $tip.attr('id') || uniqueId('ui-tooltip')
-        $tip.attr({'role':'tooltip','id' : tooltipID})
-        this.$element.attr('aria-describedby', tooltipID)
-    }
-
-    $.fn.tooltip.Constructor.prototype.hide = function () {
-        hideTooltip.apply(this, arguments)
-        removeMultiValAttributes(this.$element, 'aria-describedby', this.tip().attr('id'))
-        return this
-    }
-  // Popover Extension
-  // ===============================
-  
-  var showPopover =   $.fn.popover.Constructor.prototype.setContent
-      , hidePopover =   $.fn.popover.Constructor.prototype.hide
-
-    $.fn.popover.Constructor.prototype.setContent = function(){
-      showPopover.apply(this, arguments)
-      var $tip = this.tip()
-        , tooltipID = $tip.attr('id') || uniqueId('ui-tooltip')
-      $tip.attr({'role':'alert','id' : tooltipID})
-      this.$element.attr('aria-describedby', tooltipID)
-      this.$element.focus()
-    }
-    $.fn.popover.Constructor.prototype.hide =  function(){
-        hidePopover.apply(this, arguments)
-        removeMultiValAttributes(this.$element, 'aria-describedby', this.tip().attr('id'))
-        return this
-    }
-
   // Modal Extension
   // ===============================
 
