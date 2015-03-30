541d1ca4f27786e2061a5216dab9ac7e256ed40e
Jon Gunderson <jongund@illinois.edu>
2015-03-30 16:53:41 -0500

    updated plugin to generate code that passed OAA rules

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -7,48 +7,51 @@
           , next        = $this.find('[data-slide="next"]')
           , $tablist     = $this.find('.carousel-indicators')
           , $tabs        = $this.find('.carousel-indicators li')
+          , $tab
           , $tabpanels  = $this.find('.item')
+          , $tabpanel
           , i
+          , id = 'id_carousel'
 
         $tablist.attr('role', 'tablist')
-        $tabs.attr('role', 'tab')
         
         $tabs.focus(function() {
           $this.carousel('pause')
         });
 
-        $tabpanels.attr('role', 'tabpanel')
-        var tpa = $tabpanels.toArray();
-        for (i = 0; i < tpa.length; i++) {
-          $(tpa[i]).attr('id', 'tabpanel-' + index + '-' + i)
+        console.log('tabpanels: ' + $tabpanels + " (" + $tabpanels.length + ")")
+        for (i = 0; i < $tabpanels.length; i++) {
+          $tabpanel = $tabpanels[i]
+          $tabpanel.setAttribute('role', 'tabpanel')
+          $tabpanel.setAttribute('id', 'tabpanel-' + index + '-' + i)
+          $tabpanel.setAttribute('aria-labelledby', 'tab-' + index + '-' + i)
         }
 
         console.log("role: " + $this.attr('role') + " aria-label: " + $this.attr('aria-label'))
 
-        
-        if (typeof $this.attr('role') !== 'string') $this.attr('role', 'complementary');
-        if (typeof $this.attr('aria-labelledby') !== 'string') {
-          if (typeof $this.attr('title') !== 'string') {
-            if (typeof $this.attr('aria-label') !== 'string') $this.attr('aria-label', 'Carousel content with ' + tpa.length + ' slides');
-          }
+        if (typeof $this.attr('role') !== 'string') {
+          $this.attr('role', 'complementary');
+          $this.attr('aria-labelledby', id);
+          $this.prepend('<h2 id="' + id + '" class="sr-only">Carousel content with ' + $tabpanels.length + ' slides</h2>')
         }  
                 
-        var ta = $tabs.toArray();
-        for (i = 0; i < ta.length; i++) {
+        for (i = 0; i < $tabs.length; i++) {
           var j = i + 1;
-          $(ta[i]).attr('aria-controls', 'tabpanel-' + index + '-' + i)
+          $tab = $tabs[i]
+          $tab.setAttribute('role', 'tab')
+          $tab.setAttribute('id', 'tab-' + index + '-' + i)
+          $tab.setAttribute('aria-controls', 'tabpanel-' + index + '-' + i)
           
           var caption = $this.find('#tabpanel-' + index + '-' + i).find('h3').text()
           
           var tabName = document.createElement('span')
           tabName.setAttribute('class', 'sr-only')
-          tabName.innerHTML='Slide ' + j + " of " + ta.length
+          tabName.innerHTML='Slide ' + j
           if (caption) tabName.innerHTML += ": " +  caption
           
-          $(ta[i]).append(tabName)
+          $tab.appendChild(tabName)
         }
           
-
 
         var spanPrev = document.createElement('span')
         spanPrev.setAttribute('class', 'sr-only')
@@ -99,7 +102,7 @@
 
       $active
         .one('bsTransitionEnd', function () {
-          console.log("active: " + $active.attr('id') + " next: " + $next.attr('id'))
+//          console.log("active: " + $active.attr('id') + " next: " + $next.attr('id'))
           var $tab
           
           $tab = $element.find('li[aria-controls="' + $active.attr('id') + '"]')
@@ -114,44 +117,42 @@
 
      var $this;
      $.fn.carousel.Constructor.prototype.keydown = function (e) {
-     console.log("keydown: " + e.keyCode + " target: " + e.target)
+     
+     console.log("keydown: " + e.keyCode + " target: " + e.target.getAttribute('role'))
+     
      $this = $this || $(this)
      if(this instanceof Node) $this = $(this)
-     var $ul = $this.closest('[role=tablist]')
-      , $items = $ul.find('[role=tab]')
-      , $parent = $ul.parent()
+     
+     function selectTab(dir) {
+       if (index >= $tabs.length) return
+       if (index < 0) return
+
+       $carousel.carousel(dir)
+       setTimeout(function () {
+            $tabs[index].focus()
+            // $this.prev().focus()
+       }, 150)      
+     }
+     
+     var $carousel = $(e.target).closest('.carousel')
+      , $tabs      = $carousel.find('[role=tab]')
       , k = e.which || e.keyCode
       , index
-      , i
+
+      console.log("carousel  : " + $carousel + " (" + $carousel[0].tagName + ")")
+      console.log("tabs      : " + $tabs + " (" + $tabs.length + ")")
 
       if (!/(37|38|39|40)/.test(k)) return
-      index = $items.index($items.filter('.active'))
+      
+      index = $tabs.index($tabs.filter('.active'))
       if (k == 37 || k == 38) {                           //  Up
-        
         index--
-        if(index < 0) index = $items.length -1
-        else  {
-          $parent.carousel('prev')
-          setTimeout(function () {
-            $items[index].focus()
-            // $this.prev().focus()
-          }, 150)      
-        }  
-
+        selectTab('prev');
       }
+      
       if (k == 39 || k == 40) {                          // Down
         index++
-        if(index == $items.length) {
-          index = 0
-        }  
-        else  {
-          $parent.carousel('next')
-          setTimeout(function () {
-            $items[index].focus()
-            // $this.next().focus()
-          }, 150)            
-        }
-
+        selectTab('next');
       }
 
       e.preventDefault()
