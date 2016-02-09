808dcacf23444197480652fe008330fdc3548c9a
Chris Hallberg <crhallberg@gmail.com>
2016-02-09 14:14:11 -0500

    Compile documentation version with grunt.

--- plugins/js/bootstrap-accessibility.js
+++ plugins/js/bootstrap-accessibility.js
@@ -125,28 +125,33 @@
       , menus = $(toggle).parent().find('ul').attr('role','menu')
       , lis = menus.find('li').attr('role','presentation')
 
+    // add menuitem role and tabIndex to dropdown links
     lis.find('a').attr({'role':'menuitem', 'tabIndex':'-1'})
+    // add aria attributes to dropdown toggle
     $(toggle).attr({ 'aria-haspopup':'true', 'aria-expanded': 'false'})
 
-    $(toggle).parent().on('shown.bs.dropdown',function(e){
-      $par = $(this)
-      var $toggle = $par.find(toggle)
-      $toggle.attr('aria-expanded','true')
-      $toggle.on('keydown.bs.dropdown', $.proxy(function (ev) {
-        setTimeout(function(){
-              firstItem = $('.dropdown-menu [role=menuitem]:visible', $par)[0]
-              try{ firstItem.focus()} catch(ex) {}
-        }, focusDelay)
-      }, this))
+    $(toggle).parent()
+      // Update aria-expanded when open
+      .on('shown.bs.dropdown',function(e){
+        $par = $(this)
+        var $toggle = $par.find(toggle)
+        $toggle.attr('aria-expanded','true')
+        $toggle.on('keydown.bs.dropdown', $.proxy(function (ev) {
+          setTimeout(function() {
+            firstItem = $('.dropdown-menu [role=menuitem]:visible', $par)[0]
+            try{ firstItem.focus()} catch(ex) {}
+          }, focusDelay)
+        }, this))
 
-    })
+      })
+      // Update aria-expanded when closed
+      .on('hidden.bs.dropdown',function(e){
+        $par = $(this)
+        var $toggle = $par.find(toggle)
+        $toggle.attr('aria-expanded','false')
+      })
 
-    $(toggle).parent().on('hidden.bs.dropdown',function(e){
-      $par = $(this)
-      var $toggle = $par.find(toggle)
-      $toggle.attr('aria-expanded','false')
-    })
-
+    // Close the dropdown if tabbed away from
     $(document)
       .on('focusout.dropdown.data-api', '.dropdown-menu', function(e){
         var $this = $(this)
@@ -157,13 +162,13 @@
           return;
         }
         setTimeout(function() {
-         if(!$.contains(that, document.activeElement)){
-          $this.parent().removeClass('open')
-          $this.parent().find('[data-toggle=dropdown]').attr('aria-expanded','false')
-         }
+          if(!$.contains(that, document.activeElement)){
+            $this.parent().find('[data-toggle=dropdown]').dropdown('toggle')
+          }
         }, 150)
        })
       .on('keydown.bs.dropdown.data-api', toggle + ', [role=menu]' , $.fn.dropdown.Constructor.prototype.keydown)
+
   // Tab Extension
   // ===============================
   
