9386fb6439b57714282f58db3eaa729ae9f579a5
Becky Gibson <gibson.becky@gmail.com>
2016-07-18 11:42:59 -0400

    Fixes #69 Incorrect focus trap in modal dialog
    I modified this to keep the focus within the modal dialog as
    the user tabs and shift-tabs through the items.
    Tested with VoiceOver in Safari,
    JAWS 17 and NVDA in Firefox on Windows 10

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,7 @@
   // Modal Extension
   // ===============================
 
-	$('.modal-dialog').attr( {'role' : 'document'})
+  $('.modal-dialog').attr( {'role' : 'document'})
     var modalhide =   $.fn.modal.Constructor.prototype.hide
     $.fn.modal.Constructor.prototype.hide = function(){
        modalhide.apply(this, arguments)
@@ -10,14 +10,21 @@
 
     var modalfocus =   $.fn.modal.Constructor.prototype.enforceFocus
     $.fn.modal.Constructor.prototype.enforceFocus = function(){
-      var focEls = this.$element.find(":tabbable")
-        , lastEl = focEls[focEls.length-1]
-      $(document).on('keydown.bs.modal', $.proxy(function (ev) {
-        if(!this.$element.has(ev.target).length && ev.shiftKey && ev.keyCode === 9) {  
-          lastEl.focus()
+      var $content = this.$element.find(".modal-content")
+      var focEls = $content.find(":tabbable")
+      , $lastEl = $(focEls[focEls.length-1])
+      , $firstEl = $(focEls[0])
+      $lastEl.on('keydown.bs.modal', $.proxy(function (ev) {
+        if(ev.keyCode === 9 && !(ev.shiftKey | ev.ctrlKey | ev.metaKey | ev.altKey)) { // TAB pressed
           ev.preventDefault();
+          $firstEl.focus();
         }
       }, this))
-
+      $firstEl.on('keydown.bs.modal', $.proxy(function (ev) {
+          if(ev.keyCode === 9 && ev.shiftKey) { // SHIFT-TAB pressed
+            ev.preventDefault();
+            $lastEl.focus();
+          }
+      }, this))
       modalfocus.apply(this, arguments)
-    }    
+    }
